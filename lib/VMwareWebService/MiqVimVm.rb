2e4b9cd076ed5eea029a49af57d5bbdd1ffd9498
Adam Grare <agrare@redhat.com>
2017-05-02 13:29:59 -0400

    Allow hardware to be passed to save a getProp call
    https://bugzilla.redhat.com/show_bug.cgi?id=1445874
    
    (transferred from ManageIQ/manageiq-gems-pending@7e13304de16fd5cc2c785a6f668f3585293b4303)

--- lib/VMwareWebService/MiqVimVm.rb
+++ lib/VMwareWebService/MiqVimVm.rb
@@ -862,12 +862,14 @@ class MiqVimVm
   # Find a SCSI controller and
   # return its key and next available unit number.
   #
-  def available_scsi_units
+  def available_scsi_units(hardware = nil)
     scsi_units       = []
     all_unit_numbers = [*0..MAX_SCSI_DEVICES]
 
-    devices = getProp("config.hardware")["config"]["hardware"]["device"]
-    scsi_controllers = devices.select { |dev| VIRTUAL_SCSI_CONTROLLERS.include?(dev.xsiType) }
+    hardware ||= getHardware()
+
+    devices = hardware["device"]
+    scsi_controllers = getScsiControllers(hardware)
 
     scsi_controllers.sort_by { |s| s["key"].to_i }.each do |scsi_controller|
       # Skip if all controller units are populated
@@ -895,12 +897,10 @@ class MiqVimVm
     scsi_units
   end # def available_scsi_units
 
-  def available_scsi_buses
+  def available_scsi_buses(hardware = nil)
     scsi_controller_bus_numbers = [*0..MAX_SCSI_CONTROLLERS - 1]
 
-    devices = getProp("config.hardware")["config"]["hardware"]["device"]
-
-    scsi_controllers = devices.select { |dev| VIRTUAL_SCSI_CONTROLLERS.include?(dev.xsiType) }
+    scsi_controllers = getScsiControllers(hardware)
     scsi_controllers.each do |controller|
       scsi_controller_bus_numbers -= [controller["busNumber"].to_i]
     end
@@ -912,10 +912,10 @@ class MiqVimVm
   # Returns the [controllerKey, key] pair for the virtul device
   # associated with the given backing file.
   #
-  def getDeviceKeysByBacking(backingFile)
-    devs = getProp("config.hardware")["config"]["hardware"]["device"]
+  def getDeviceKeysByBacking(backingFile, hardware = nil)
+    hardware ||= getHardware()
 
-    devs.each do |dev|
+    hardware["device"].to_a.each do |dev|
       next if dev.xsiType != "VirtualDisk"
       next if dev["backing"]["fileName"] != backingFile
       return([dev["controllerKey"], dev["key"]])
